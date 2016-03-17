2c75e3888494a4ef5c8b7ded7d29b9b281736a46
sreadbrown <sreadbrown@gmail.com>
2016-03-17 13:56:00 -0700

    will add the html and js for travel buddy and noted questions

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@
 function Journal (location, year, buddy, notes) {
   this.location = location;
   this.yearVisited = year;
-  // this.travelBuddy = buddy;
-  // this.notes = notes;
+  this.travelBuddy = buddy;
+  this.notes = notes;
 }
 
 $(document).ready(function() {
@@ -12,12 +12,12 @@ $(document).ready(function() {
 
     var inputtedLocation = $("input#new-location").val();
     var inputtedYear = $("input#new-year").val();
-    // var inputtedBuddy = $("input#new-buddy").val();
-    // var inputtedNotes = $("input#new-notes").val();
+    var inputtedBuddy = $("input#new-buddy").val();
+    var inputtedNotes = $("input#new-notes").val();
 
-    // var newJournalEntry = new Journal(inputtedLocation, inputtedYear, inputtedBuddy, inputtedNotes);
+    var newJournalEntry = new Journal(inputtedLocation, inputtedYear, inputtedBuddy, inputtedNotes);
 
-    var newJournalEntry = new Journal(inputtedLocation, inputtedYear);
+    // var newJournalEntry = new Journal(inputtedLocation, inputtedYear);
 
     $("ul#entries").append("<li><span class='entry'>" + newJournalEntry.location + "</span></li>");
 
@@ -26,14 +26,14 @@ $(document).ready(function() {
       $("#show-journal h2").text(newJournalEntry.location);
       $(".location").text(newJournalEntry.location);
       $(".year-visited").text(newJournalEntry.yearVisited);
-      // $(".travel-buddy").text(newJournalEntry.travelBuddy);
-      // $(".notes").text(newJournalEntry.notes);
+      $(".travel-buddy").text(newJournalEntry.travelBuddy);
+      $(".notes").text(newJournalEntry.notes);
     });
 
     $("input#new-location").val("");
     $("input#new-year").val("");
-    // $("input#new-buddy").val("");
-    // $("input#new-notes").val("");
+    $("input#new-buddy").val("");
+    $("input#new-notes").val("");
 
   });
 });
